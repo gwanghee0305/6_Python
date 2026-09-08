6f0fc3ccb166d0566cd91f2e584d4df2fdd60c37
gwanghee <mm2pr2645@gmail.com>
2026-09-08 17:26:56 +0900

    0908 커밋

---
+++ 02_data_structures/01_list.py
@@ -0,0 +1,53 @@
+"""
+    리스트(list)
+"""
+
+# 리스트 데이터 표현 : 대괄호 사용 [] 
+colors = ["red", "green", "blue"]
+
+print(f"colors -> {colors}")
+# 첫 번째 요소 출력
+print(f"첫 번째 : {colors[0]}")
+# 마지막 요소 출력
+print(f"마지막 : {colors[2]}")
+print(f"마지막 : {colors[-1]}")
+
+print(f"{colors[0:2]}")
+
+# 다양한 타입의 데이터를 담을 수 있음
+mixed = [100, "Hello", True, [1,2,3]]
+print(f"mixed : {mixed}")
+
+# 리스트 상태에 따라 bool 타입 확인
+temp = []
+print(f"mixed --> {bool(mixed)}")
+print(f"temp --> {bool(temp)}")
+
+print("=" * 60)
+items = ["페페로니", "고구마", "포테이토"]
+
+print(f"items --> {items}")
+
+# 데이터 추가 : append(), insert(), extend()
+items.append("불고기")
+print(f"append - 맨 뒤에 추가 : {items}")
+
+items.insert(2, "콤비")
+print(f"insert - 위치를 지정해서 추가: {items}")
+
+items.extend(["꼬북칭","허니버터"])
+print(f"extend")
+
+# 수정 / ㅅ삭제
+print("=" * 60)
+items[0] = "ACE"
+print(f"특정 인덱스를 지엉하여 값을 견경 :{items}")
+
+items.remove("고구마")
+print(f"특정 인덱스를 지엉하여 값을 견경 :{items}")
+
+snack = items.pop()
+print(f"pop - {snack} / {items}")    # 맨뒤에 데이터를 삭제 후 반환
+
+del items[0]
+print(f"del - 인덱스로 삭제: {items}")
